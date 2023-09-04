e609fa1c2130d6f8889628ab2af9b9d28eadb898
abdulalimrakib <abdulalimrakib04@gmail.com>
2023-09-04 22:04:13 +0600

    API half done

---
+++ script.js
@@ -0,0 +1,69 @@
+
+
+fetch(`https://openapi.programming-hero.com/api/ai/tools`)
+    .then((response) => response.json())
+    .then((value) => {
+        
+       
+        // let features = [];
+        // for (let i = 0; i < 3; i++) {
+        //     features.push(`${i + 1}. ${data[0].features[i]}`)
+        // }
+
+        
+        
+
+        
+
+        // div.innerHTML = `
+        //     <div class="border-2 p-3 shadow-md">
+        //         <img class="rounded-md" src="${data[0].image}" alt="">
+        //         <h3 class="py-2 font-bold text-[20px] text-start">Features</h3>
+        //         <ul class="text-start">
+        //             <li>${features[0]}</li>
+        //             <li>${features[1]}</li>
+        //             <li>${features[2]}</li>
+        //         </ul>
+        //         <hr class="my-3 mx-6 border-1 border-gray-300">
+        //         <h2 class="font-bold text-[20px] text-start">${data[0].name}</h2>
+        //         <p class="text-start"><i class="fa-solid fa-calendar-days pr-2"></i> ${data[0].published_in}</p>
+        //     </div>
+        // `
+        let div = document.getElementById("result");
+        for (let id = 0; id < 6; id++) {
+            if(id==5)
+            id=7;
+            let data = value.data.tools[id];
+            let newDiv = document.createElement("div");
+            newDiv.classList.add("border-2", "p-5", "shadow-md", "rounded-lg", "transform", "hover:scale-105", "transition", "duration-300")
+
+             console.log(data)
+            // console.log(data.image)
+            // console.log(data.name);
+            // console.log(data.published_in);
+
+             let features = [];
+                for (let i = 0; i < 3; i++) {
+                    features.push(`${i + 1}. ${data.features[i]}`)
+                }
+                //  console.log(features);
+
+            newDiv.innerHTML=`
+                    <a class="transform hover:scale-105 transition duration-300" href="">
+                        <img class="rounded-md max-h-[200px] min-w-full" src="${data.image}" alt="">
+                        <h3 class="py-2 font-bold text-[20px] text-start">Features</h3>
+                        <ul class="text-start">
+                            <li>${features[0]}</li>
+                            <li>${features[1]}</li>
+                            <li>${features[2]}</li>
+                        </ul>
+                        <hr class="my-3 mx-6 border-1 border-gray-300">
+                        <h2 class="font-bold text-[20px] text-start">${data.name}</h2>
+                        <p class="text-start"><i class="fa-solid fa-calendar-days pr-2"></i> ${data.published_in}</p>
+                    </a>
+                `;
+            div.appendChild(newDiv)
+        }
+
+
+    })
